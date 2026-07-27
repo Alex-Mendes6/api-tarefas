d2c924f0092255a3c6fd202188cdb7cfe862bc6b
Alex Mendes Queiroz <alexqueirozmendes@gmail.com>
2026-07-27 10:55:44 -0300

    feat(all): cria as rotas basicas e atualiza os tipos

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const PORTA = 3333;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ mensagem: 'API funcionando!' });
+});
+
+app.post('/', (req, res) => {
+    res.status(200).send('Recebemos seu POST! Obrigado!');
+});
+
+app.listen(PORTA, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORTA}`);
+});
