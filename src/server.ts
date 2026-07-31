33c6e69e85cb2ba38fcd350ef2fbe78bfe536e13
Alex Mendes Queiroz <alexqueirozmendes@gmail.com>
2026-07-31 18:07:13 -0300

    feat(config): adiciona as configuracoes do prismaClient e do express

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import express from 'express';
-import { tarefaRoutes } from './routes/tarefa.routes.js';
+// src/index.ts ← apenas sobe o servidor
+import { app } from './config/expressConfig';
 
-const app = express();
 const PORTA = 3333;
 
-app.use(express.json());
-
-app.use('/tasks', tarefaRoutes);
-
 app.listen(PORTA, () => {
-    console.log(`🚀 Servidor rodando na porta ${PORTA}`);
+  console.log(`🚀 Servidor rodando na porta ${PORTA}`);
 });
